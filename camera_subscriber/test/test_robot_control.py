de88df6834aa0a637cf47e9bf0ba8e8df740f766
Maciej <maciej.krupka@rad4m.com>
2023-01-24 17:32:03 +0100

    fix: Add unit test

---
+++ camera_subscriber/test/test_robot_control.py
@@ -0,0 +1,50 @@
+import math
+import unittest
+
+import robot_control
+
+class TestQuaternionMethods(unittest.TestCase):
+
+    def test_rad_to_deg(self):
+        self.assertEqual(my_quaternion.rad_to_deg(my_quaternion.math.pi), 180.0)
+
+    def test_quat_to_euler(self):
+        quaternion = [my_quaternion.math.cos(math.pi/2.0), 0.0, 0.0, -my_quaternion.math.sin(math.pi/4.0)]
+        euler = my_quaternion.quat_to_euler(quaternion)
+        euler_round = list(map(round, euler))
+        self.assertListEqual(euler_round, [-90, 0, 0])
+
+    def test_normalize_quat(self):
+        quaternion = [0.0, 0.0, 1.7071, -0.70711]
+        normalize_quaternion = [0.0, 0.0, 0.92388, -0.38268]
+        normalize_quaternion = list(map(lambda x: round(x,4), normalize_quaternion))
+        quaternion = my_quaternion.normalize_quat(quaternion)
+        quaternion = list(map(lambda x: round(x, 4), quaternion))
+        self.assertListEqual(quaternion, normalize_quaternion)
+
+    def test_inv_quat(self):
+        quaternion = [ my_quaternion.math.cos(math.pi/4.0), 0.0, 0.0, -my_quaternion.math.sin(math.pi/4.0)]
+        expectation = [ -my_quaternion.math.cos(math.pi/4.0), 0.0, 0.0, -my_quaternion.math.sin(math.pi/4.0)]
+        self.assertListEqual( my_quaternion.inv_quat(quaternion), expectation)
+
+    def test_multiplication_quat(self):
+        quaternion1 = [0.0, 0.0, -0.70711, 0.70711]
+        quaternion2 = [0, 0, -0.38268, 0.92388]
+        expectation = [0, 0, -0.92388, 0.38268]
+        expectation = list(map(lambda x: round(x, 4), expectation))
+        result = my_quaternion.multiplication_quat(quaternion1, quaternion2)
+        result = list(map(lambda x: round(x, 4), result))
+        self.assertListEqual(result, expectation)
+
+    def test_get_changed_orientation(self):
+        expectation = [0.0, 0.0, -0.70711, 0.70711]
+        expectation = list(map(lambda x: round(x, 4), expectation))
+        quaternion1 = [0, 0, -0.38268, 0.92388]
+        quaternion2 = [0, 0, -0.92388, 0.38268]
+        result = my_quaternion.get_changed_orientation(quaternion2, quaternion1)
+        result = list(map(lambda x: round(x, 4), result))
+        self.assertListEqual(result, expectation)
+
+
+if __name__ == '__main__':
+    unittest.main()
